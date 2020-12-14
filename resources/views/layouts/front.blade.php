a85a1876511a69036b50b14199c5249342b7450b
mg-abramishvili <mg@abramishvili.net>
2020-12-14 19:30:42 +0500

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Техосмотр</title>
 
+    <link rel="stylesheet" href="/css/style.css">
+
     <script src="/js/jquery-3.4.1.min.js"></script>
 </head>
 <body>
