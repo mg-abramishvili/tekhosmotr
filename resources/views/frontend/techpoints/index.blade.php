af1a94a5bf791fcce2b1c4c8ced464bc0e27a9eb
mg-abramishvili <mg@abramishvili.net>
2020-12-28 14:02:54 +0500

    .

--- resources/views/frontend/techpoints/index.blade.php
+++ resources/views/frontend/techpoints/index.blade.php
@@ -3,20 +3,23 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12 col-lg-3">
-                <div class="main-techpoints-list">
+            <div class="col-12 col-lg-3 left-panel">
+                <div class="search">
+                    <input type="text" class="form-control" id="myInput" onkeyup="myFunction()" placeholder="Поиск по названию, адресу...">
+                </div>
+                <div class="main-techpoints-list" id="main-techpoints-list">
                     @foreach($techpoints as $techpoint)
                     <div class="main-techpoints-list-item">
                         <a href="/techpoints/{{ $techpoint->id }}">
                             <p class="title">{{ $techpoint->title }}</p>
-                            <span class="address">Адрес: <strong>{{ $techpoint->address }}</strong></span>
-                            <span class="tel">Телефон: <strong>{{ $techpoint->tel }}</strong></span>
+                            <span class="address">{{ $techpoint->address }}</span>
+                            <span class="tel">{{ $techpoint->tel }}</span>
                         </a>
                     </div>
                     @endforeach
                 </div>
             </div>
-            <div class="col-12 col-lg-9">
+            <div class="col-12 col-lg-9 right-panel">
                 <div class="main-map">
                     <div id="map"></div>
                 </div>
@@ -65,4 +68,23 @@
             myMap.controls.remove('trafficControl');
         });
     </script>
+
+<script>
+    function myFunction() {
+        var input, filter, ul, li, a, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("main-techpoints-list");
+        li = ul.getElementsByTagName("div");
+        for (i = 0; i < li.length; i++) {
+            a = li[i].getElementsByTagName("a")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+</script>
 @endsection
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -8,26 +8,31 @@
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
 
-    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/jquery-3.5.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 </head>
 <body>
 
     <header>
         <div class="container">
             <div class="row align-items-center">
-                <div class="col-12 col-lg-3 header-logo">
+                <div class="col-12 col-lg-2 header-logo">
                     <a href="/techpoints">LOGO</a>
+                    <button data-toggle="modal" data-target="#myModal">Уфа</button>
                 </div>
-                <div class="col-12 col-lg-9 header-nav">
+                <div class="col-12 col-lg-10 header-nav">
                     <nav class="navbar navbar-expand-lg">
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNavDropdown">
                             <ul class="nav navbar-nav">
-                                <li class="nav-item"><a href="#" class="nav-link">Пункт меню</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Пункт меню</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Пункт меню</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link"><span>Аккредитация</span></a></li>
+                                <li class="nav-item"><a href="#" class="nav-link"><span>Законодательство</span></a></li>
+                                <li class="nav-item"><a href="#" class="nav-link"><span>Метрологическое сопрвождение</span></a></li>
+                                <li class="nav-item"><a href="#" class="nav-link"><span>Оборудование для ТО</span></a></li>
+                                <li class="nav-item"><a href="#" class="nav-link"><span>Ремонт оборудования</span></a></li>
+                                <li class="nav-item"><a href="#" class="nav-link"><span>Контакты</span></a></li>
                             </ul>
                         </div>
                     </nav>
@@ -37,6 +42,43 @@
     </header>
 
     @yield('content')
+
+<div class="modal" id="myModal">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <ul>
+            <li><a href="#">Уфа</a></li>
+            <li><a href="#">Агидель</a></li>
+            <li><a href="#">Баймак</a></li>
+            <li><a href="#">Белебей</a></li>
+            <li><a href="#">Белорецк</a></li>
+            <li><a href="#">Бирск</a></li>
+            <li><a href="#">Благовещенск</a></li>
+            <li><a href="#">Давлеканово</a></li>
+            <li><a href="#">Дюртюли</a></li>
+            <li><a href="#">Ишимбай</a></li>
+            <li><a href="#">Кумертау</a></li>
+            <li><a href="#">Межгорье</a></li>
+            <li><a href="#">Мелеуз</a></li>
+            <li><a href="#">Нефтекамск</a></li>
+            <li><a href="#">Октябрьский</a></li>
+            <li><a href="#">Приютово</a></li>
+            <li><a href="#">Салават</a></li>
+            <li><a href="#">Сибай</a></li>
+            <li><a href="#">Стерлитамак</a></li>
+            <li><a href="#">Туймазы</a></li>
+            <li><a href="#">Учалы</a></li>
+            <li><a href="#">Чишмы</a></li>
+            <li><a href="#">Янаул</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
 
     @yield('scripts')
 </body>
