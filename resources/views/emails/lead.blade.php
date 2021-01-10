d6d90cd60362cbb88b17c838f201bfbae1be5107
mg-abramishvili <mg@abramishvili.net>
2021-01-10 20:40:26 +0500

    .

--- resources/views/emails/lead.blade.php
+++ resources/views/emails/lead.blade.php
@@ -1,6 +1,18 @@
 @component('mail::message')
 # Новая заявка с сайта БашТехОсмотр.РФ
 
-{{ $station }}
+Станция:<br>{{ $station }}
+<br><br>
+
+Дата:<br>{{ $date }}
+<br><br>
+
+Время:<br>{{ $time }}
+<br><br>
+
+Имя:<br>{{ $name }}
+<br><br>
+
+Телефон:<br>{{ $phone }}
 
 @endcomponent
