81addf2cbdca13678fda721f84bb33319d2e5017
mg-abramishvili <mg@abramishvili.net>
2021-01-10 18:42:12 +0500

    .

--- resources/views/frontend/pages/show.blade.php
+++ resources/views/frontend/pages/show.blade.php
@@ -7,7 +7,18 @@
             <h1>{{ $page->title }}</h1>
         </div>
 
-        {!! $page->text !!}
+        @if($page->slug == 'kontakty')
+        <div class="row">
+            <div class="col-12 col-md-6">
+                {!! $page->text !!}
+            </div>
+            <div class="col-12 col-md-6">
+                <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A553f255ca7d407bf229ba2ff15efd55960c887e302369a2b09c015cf2e4dc4c5&amp;width=100%25&amp;height=350&amp;lang=ru_RU&amp;scroll=false"></script>
+            </div>
+        </div>
+        @else
+            {!! $page->text !!}
+        @endif
 
     </div>
 
