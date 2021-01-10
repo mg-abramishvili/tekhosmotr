661c2af6ff9f5c45fff68a5dd311eb5dfff9c096
mg-abramishvili <mg@abramishvili.net>
2021-01-10 19:03:51 +0500

    .

--- resources/views/frontend/pages/show.blade.php
+++ resources/views/frontend/pages/show.blade.php
@@ -10,10 +10,10 @@
         @if($page->slug == 'kontakty')
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-md-6 page-content-left">
                     {!! $page->text !!}
                 </div>
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-md-6 page-content-right">
                     <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A553f255ca7d407bf229ba2ff15efd55960c887e302369a2b09c015cf2e4dc4c5&amp;width=100%25&amp;height=350&amp;lang=ru_RU&amp;scroll=false"></script>
                 </div>
             </div>
