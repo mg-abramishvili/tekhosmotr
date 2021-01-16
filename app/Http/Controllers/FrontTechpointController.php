b32dc3becf50c4d84fdacaf5904154bc0d4ca079
mg-abramishvili <mg@abramishvili.net>
2021-01-16 15:07:19 +0500

    .

--- app/Http/Controllers/FrontTechpointController.php
+++ app/Http/Controllers/FrontTechpointController.php
@@ -14,7 +14,7 @@ class FrontTechpointController extends Controller
 {
     public function index($city, Request $request)
     {
-        $goroda = City::sortBy('sort', 'asc')->get();
+        $goroda = City::sortBy('sort');
         $techpoints = Techpoint::whereHas('cities', function ($query) use($city) {
             return $query->where('city_code', '=', $city);
         })
