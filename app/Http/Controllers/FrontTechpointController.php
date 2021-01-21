fd0c664e81ee5681eaf5112a441f3001f6ed356d
mg-abramishvili <mg@abramishvili.net>
2021-01-21 19:26:36 +0500

    .

--- app/Http/Controllers/FrontTechpointController.php
+++ app/Http/Controllers/FrontTechpointController.php
@@ -68,7 +68,7 @@ class FrontTechpointController extends Controller
 
         $lead = Lead::find($leads->id);
 
-        Mail::to($lead->email)->send(new NewLead($lead));
+        Mail::to('mg@abramishvili.net')->send(new NewLead($lead));
         
         return redirect()->back();
     }
